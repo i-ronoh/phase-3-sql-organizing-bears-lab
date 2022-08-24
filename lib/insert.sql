7e26804257901d64bcd089ba4c61117a60f4222a
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-08-24 16:41:46 +0300

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 15, "M", "Brown", "Jittery", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 7, "M", "Dark Brown", "Shy", 0);
+INSERT INTO bears VALUES (3, "Tabitha", 30, "F","Light Brown", "Fierce", 0);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 36, "M", "Brown", "Sad", 1);
+INSERT INTO bears VALUES (5, "Melissa", 17, "F", "Mustard", "Jolly", 0);
+INSERT INTO bears VALUES (6, "Grinch", 28, "M", "Green", "Angry", 1);
+INSERT INTO bears VALUES (7, "Wendy", 2, "F", "Grey", "Happy", 0);
+INSERT INTO bears VALUES (8, NULL, 11, "F", "Brown", "Intense", 0);
+
+
